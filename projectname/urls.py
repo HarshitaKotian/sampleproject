0c139762db2f230162208bdb48036571da382cd1
HarshitaKotian <harshithap897@gmail.com>
2023-05-05 14:46:26 +0530

    ok

--- projectname/urls.py
+++ projectname/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
-    path('products/',views.products,name='products')
+    path('products/',views.products,name='products'),
 ]
